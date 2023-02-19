6a2fbd260b5e6afa115b0fd5898ee80bb290ceea
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-19 18:52:31 +0000

    feat: add challenge to importing-components route

---
+++ app/routes/memberships.tsx
@@ -0,0 +1,52 @@
+interface MembershipCardProps {
+  level: string;
+  description: string;
+  price: string;
+}
+
+function MembershipCard({ level, description, price }: MembershipCardProps) {
+  return (
+    <li className="mb-4 rounded-lg bg-white p-4 text-center shadow-lg transition-all last-of-type:mb-0 hover:scale-105 hover:cursor-pointer">
+      <h2 className="mb-4 font-serif text-2xl font-extrabold tracking-wide text-green-900">
+        {level}
+      </h2>
+      <dl>
+        <dt className="bold mb-1 text-xs uppercase text-amber-900">
+          Description
+        </dt>
+        <dd className="mb-3 text-neutral-600">{description}</dd>
+        <dt className="bold mb-1 text-xs uppercase text-amber-900">Price</dt>
+        <dd className="mb-3 font-serif text-xl font-extrabold text-red-800">
+          {price}
+        </dd>
+      </dl>
+    </li>
+  );
+}
+
+export default function Memberships() {
+  return (
+    <div className="min-h-screen bg-amber-200 p-4">
+      <h1 className="mb-4 text-center font-serif text-4xl font-extrabold text-red-900">
+        Memberships
+      </h1>
+      <ul className="mx-auto max-w-xl p-4">
+        <MembershipCard
+          level="Basic"
+          description="1 martial art - 2 sessions per week. Monthly fee."
+          price="£25.00"
+        />
+        <MembershipCard
+          level="Intermediate"
+          description="1 martial art - 3 sessions per week. Monthly fee."
+          price="£35.00"
+        />
+        <MembershipCard
+          level="Advanced"
+          description="Any 2 martial arts - 5 sessions per week. Monthly fee."
+          price="£45.00"
+        />
+      </ul>
+    </div>
+  );
+}
--- app/routes/remix/routing-in-remix/adding-a-new-page/importing-components.tsx
+++ app/routes/remix/routing-in-remix/adding-a-new-page/importing-components.tsx
@@ -1,11 +1,72 @@
+import { basicMembershipSnippet } from "~/assets/codeSnippets/addMembershipsRoute/membershipRouteSnippets";
+import ChallengeBlock from "~/components/ChallengeBlock";
+import CodePlayground from "~/components/CodePlayground";
+import Heading from "~/components/Heading";
+import InlineMonoType from "~/components/InlineMonoType";
+import Paragraph from "~/components/Paragraph";
+import Screenshot from "~/components/Screenshot";
+import callingMembershipComponent from "~/assets/calling-membership-component.png";
+
 interface ImportingComponentsProps {
   children: React.ReactNode;
 }
 
+const membershipRouteFile = {
+  "/App.tsx": `import Membership from './membership.tsx';
+
+  export default function App(): JSX.Element {
+    return <Membership />;
+  }
+  `,
+  "/membership.tsx": { active: true, code: basicMembershipSnippet },
+};
+
 const ImportingComponents = ({ children }: ImportingComponentsProps) => {
   return (
     <>
-      <p>ImportingComponents </p>
+      <Heading isCentre level="h2">
+        Using Custom Components
+      </Heading>
+      <Paragraph darkMode>
+        React components allow you to bundle repeated code.
+      </Paragraph>
+      <Paragraph darkMode>
+        Look again at the code in the code you imported into your new route file
+        at <InlineMonoType darkMode>app/routes/memberships.tsx</InlineMonoType>:
+      </Paragraph>
+
+      <ChallengeBlock>
+        <Paragraph challengeBlock>
+          There is a LOT of repeated code below. Can you see where these blocks
+          of repeated code start and finish?
+        </Paragraph>
+        <Paragraph challengeBlock>
+          Complete the{" "}
+          <InlineMonoType challengeBlock>MembershipCard</InlineMonoType>{" "}
+          component at the top of the{" "}
+          <InlineMonoType challengeBlock>memberships.tsx</InlineMonoType> file
+          below that allows you to pass in the props of{" "}
+          <InlineMonoType challengeBlock>level</InlineMonoType>,{" "}
+          <InlineMonoType challengeBlock>description</InlineMonoType> and{" "}
+          <InlineMonoType challengeBlock>price</InlineMonoType>
+        </Paragraph>
+        <Paragraph challengeBlock>
+          When finished, you should be able to call the{" "}
+          <InlineMonoType challengeBlock>MembershipCard</InlineMonoType>{" "}
+          component like so:
+        </Paragraph>
+        <Screenshot
+          src={callingMembershipComponent}
+          alt="Calling the Membership component"
+          challengeBlock
+        />
+      </ChallengeBlock>
+
+      <CodePlayground
+        title="Memberships.tsx"
+        files={membershipRouteFile}
+        showTabs={false}
+      />
     </>
   );
 };
