295eb65d6eed2c175ec5a39acd6bc8dd9a480055
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-19 18:51:04 +0000

    style: fix spacing on SandpackForkBtn component

--- app/components/CodePlayground/SandpackForkBtn.tsx
+++ app/components/CodePlayground/SandpackForkBtn.tsx
@@ -4,10 +4,10 @@ import Boop from "../Boop";
 
 const SandpackForkBtn = () => {
   return (
-    <UnstyledOpenInCodeSandboxButton className="py-2">
-      <Boop rotation={12} scale={1.1}>
+    <UnstyledOpenInCodeSandboxButton className="ml-1 mr-2">
+      <Boop rotation={8} scale={1.1}>
         <Codesandbox
-          className={`ml-2 stroke-neutral-400 shadow-sm group-hover:stroke-neutral-200`}
+          className={`h-auto w-4 stroke-neutral-400 hover:stroke-neutral-200`}
         />
       </Boop>
     </UnstyledOpenInCodeSandboxButton>
