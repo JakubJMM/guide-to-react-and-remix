cde732a0a14234b7d20cce92450a437a785c5ed3
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-22 16:55:33 +0000

    style: add icon to ExternalLink component

--- app/components/ExternalLink.tsx
+++ app/components/ExternalLink.tsx
@@ -1,17 +1,29 @@
+import { ExternalLink as ExternalLinkIcon } from "react-feather";
+
 interface ExternalLinkProps {
+  challengeBlock?: boolean;
   children: React.ReactNode;
   to: string;
 }
 
-const ExternalLink = ({ children, to }: ExternalLinkProps) => {
+const ExternalLink = ({
+  challengeBlock = false,
+  children,
+  to,
+}: ExternalLinkProps) => {
   return (
     <a
-      className="text-blue-500 underline hover:text-blue-700"
+      className={`inline-flex items-center text-blue-500 underline no-underline ${
+        challengeBlock ? "hover:text-blue-300" : "hover:text-blue-700"
+      }`}
       href={to}
       target="_blank"
       rel="noopener noreferrer"
     >
-      {children}
+      {children}{" "}
+      <span className="ml-2">
+        <ExternalLinkIcon />
+      </span>
     </a>
   );
 };
