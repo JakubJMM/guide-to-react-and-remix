f9a22e223c15745bd0e65351a93ab6544558b8c5
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-03-05 19:16:17 +0000

    feat: Add welcome message to Remix index

---
+++ app/routes/remix/index.tsx
@@ -0,0 +1,13 @@
+function Remix() {
+  return (
+    <div className="relative">
+      <div>
+        <h1 className="mt-16 text-center text-4xl font-black leading-relaxed text-white">
+          Pick a lesson from the list to begin...
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Remix;
