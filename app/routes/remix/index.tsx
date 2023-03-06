dd4be3f718b127cc47e3979dee259ee00b4856c7
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-03-06 11:54:47 +0000

    style: add graphic to remix index

--- app/routes/remix/index.tsx
+++ app/routes/remix/index.tsx
@@ -1,3 +1,5 @@
+import { Smile } from "react-feather";
+
 function Remix() {
   return (
     <div className="relative">
@@ -5,6 +7,12 @@ function Remix() {
         <h1 className="mt-16 text-center text-4xl font-black leading-relaxed text-white">
           Pick a lesson from the list to begin...
         </h1>
+
+        <Smile
+          className="mx-auto mt-8 stroke-amber-200"
+          size="8rem"
+          strokeWidth={1}
+        />
       </div>
     </div>
   );
